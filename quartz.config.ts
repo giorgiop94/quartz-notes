389567cc036c335c7e17fb14cfec1fc7ff50b2ce
giorgio <parlato.giorgio@gmail.com>
2024-03-18 19:57:35 +0100

    change configuration

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "spoken_g's notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
@@ -22,8 +22,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Raleway",
+        body: "Lora",
         code: "IBM Plex Mono",
       },
       colors: {
@@ -33,8 +33,8 @@ const config: QuartzConfig = {
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
+          secondary: "#4785FD",
+          tertiary: "#22aaa1",
           highlight: "rgba(143, 159, 169, 0.15)",
         },
         darkMode: {
@@ -43,8 +43,8 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
+          secondary: "#4785FD",
+          tertiary: "#22aaa1",
           highlight: "rgba(143, 159, 169, 0.15)",
         },
       },
